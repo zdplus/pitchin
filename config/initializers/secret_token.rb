ea2483238d366953348acf13143f550c3bdd5509
ZD Yu <gg.fish@gmail.com>
2011-09-06 21:13:23 -0400

    Added rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pitchin::Application.config.secret_token = '9cacb4a0d7c331136f2e365f7df28372dfb5c50c27570c4b2d9ec07c6d3aa4a874debd6982a07a2abc77270dbbcf2a772d416800e351f1eaef2378c5efc1fa0c'
